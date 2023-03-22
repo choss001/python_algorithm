28590efa1d6c6ecad7f2478edf26e8a8c80be95a
JSS <choss01@naver.com>
2023-03-22 20:45:27 +0900

    test

--- test.py
+++ test.py
@@ -14,3 +14,13 @@ for num in range(2, 10):
 		print("Found an even number", num)
 		coutinue
 	print("Found an odd number", num)
+
+fruits = ['orange', 'apple', 'pear', 'banana', 'kiwi', 'apple', 'banana']
+fruits.count('apple')
+fruits.count('tangerine')
+fruits.index('banana')
+fruits.index('banana', 4) 
+fruits.reverse()
+fruits.append('grape')
+fruits.sort()
+fruits.pop()
