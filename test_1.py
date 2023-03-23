e540c024380d698c784d549172e94cc714212069
Jss91 <choss002@gmail.com>
2023-03-23 15:57:37 +0900

    python Nested List Comprehensions practice

--- test_1.py
+++ test_1.py
@@ -1,3 +1,22 @@
 year = 2016
 event = 'Referendum'
 f'Results of the {year} {event}'
+
+
+transposed = []
+for i in range(4):
+    # the following 3 lines implement the nested listcomp
+    transposed_row = []
+    for row in matrix:
+        transposed_row.append(row[i])
+    transposed.append(transposed_row)
+
+transposed
+
+transposed = []
+for i in range(4):
+    transposed.append([row[i] for row in matrix])
+
+transposed
+
+[[row[i] for row in matrix] for i in range(4)]
