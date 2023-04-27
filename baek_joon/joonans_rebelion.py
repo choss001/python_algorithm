be76845f305fbd8fbafa5912855bd795c8d4c871
choss001 <choss01@naver.com>
2023-04-27 18:38:51 +0900

    turn on the light

--- baek_joon/joonans_rebelion.py
+++ baek_joon/joonans_rebelion.py
@@ -1,16 +1,35 @@
 from collections import deque
 
-input_map = [
-['1','#','1','0','1','1','1'],
-['1','1','0','1','0','0','1'],
-['0','0','1','*','1','1','1'],
-['1','1','0','1','1','1','1'],
-['0','0','1','1','0','0','1'],
-]
+#input_map = [
+#    ['1','#','1','0','1','1','1'],
+#    ['1','1','0','1','0','0','1'],
+#    ['0','0','1','*','1','1','1'],
+#    ['1','1','0','1','1','1','1'],
+#    ['0','0','1','1','0','0','1'],
+#]
+#
+#h, w = 5, 7
+#ty1, tx1 = 3, 4
+#ty2, tx2 = 1, 2
 
-h, w = 5, 7
-ty1, tx1 = 3, 4
-ty2, tx2 = 1, 2
+
+
+
+#h, w = 3, 5
+#ty1, tx1 = 3, 5 
+#ty2, tx2 = 1, 1
+#input_map = [
+#['#','0','0','0','0'],
+#['1','1','1','1','1'],
+#['0','0','0','0','*']
+#]
+h, w = map(int, input().split())
+temp = list(map(int, input().split()))
+ty1, tx1 = temp[0], temp[1]
+ty2, tx2 = temp[2], temp[3]
+
+input_map = [list(input().rstrip()) for _ in range(h)]
+
 
 d = [[-1, 0],[0, 1],[1, 0],[0, -1]]
 visited = [[False] * w for _ in range(h)]
@@ -19,13 +38,13 @@ y1, x1 = ty1 -1, tx1 -1
 y2, x2 = ty2 -1, tx2 -1
 
 
-q = deque((y1, x1, 0))
+q = deque([(y1, x1, 1)])
+visited[y1][x1] = True
 
 answer = 1e9
 
 while(q):
-    print(f'q = {q}')
-    [y, x, c]= q.popleft()
+    y, x, c = q.popleft()
 
     for i in d:
         dy, dx = y + i[0] , x + i[1]
@@ -38,6 +57,5 @@ while(q):
                 visited[dy][dx] = True
             else:
                 answer = min(answer, c)
-
 
 print(answer)
---
+++ baek_joon/turn_on_the_light_bulb_2423.py
@@ -0,0 +1,11 @@
+
+n,m = 3, 5
+#input_map= [
+#['\\','\\','/','\\','\\'],
+#['\\','\\','/','/','/'],
+#['/','\\','\\','\\','\\'],
+#]
+input_map = [list(input().rstrip())  for _ in range(n)]
+
+for i in input_map:
+    print(i)
