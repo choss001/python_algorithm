36066f4de5911a017171752487b8d6c4f31e7b56
choss001 <choss01@naver.com>
2023-09-18 18:03:02 +0900

    islands number algorithm finish!!

--- baek_joon/dfs/islands_number_4963.py
+++ baek_joon/dfs/islands_number_4963.py
@@ -1,13 +1,44 @@
 import sys
 input = sys.stdin.readline
+sys.setrecursionlimit(10**6)
+
+move_x = [0, 1, 1, 1, 0, -1, -1, -1]
+move_y = [-1, -1, 0, 1, 1, 1, 0, -1]
+
+def dfs(sx, sy, visited_check, mx, my, input_list):
+    for move_coordinate in range(8):
+        dx = sx + move_x[move_coordinate]
+        dy = sy + move_y[move_coordinate]
+
+        if dx <0 or dy < 0 or dx >= mx or dy >= my:
+            continue
+        if visited_check[dy][dx] == 1:
+            continue
+        if input_list[dy][dx] == 0:
+            continue
+        visited_check[dy][dx] = 1
+        dfs(dx, dy, visited_check, mx, my, input_list)
 
 
-ix, iy = map(int, input().split())
 
-print(f'{ix} {iy}')
-input_list = []
-for dy in range(iy):
-    ix_list = list(map(int, input().split()))
-    input_list.append(ix_list)
+while True:
+    ix, iy = map(int, input().split())
+    if ix == 0 and iy == 0:
+        break
 
-print(f'{input_list}')
+    input_list = []
+    for dy in range(iy):
+        ix_list = list(map(int, input().split()))
+        input_list.append(ix_list)
+
+    visited_check = [[0] * ix for _ in range(iy)]
+    answer = 0
+    for sy in range(iy):
+        for sx in range(ix):
+            if visited_check[sy][sx] == 0 and input_list[sy][sx] == 1:
+                answer += 1
+                dfs(sx, sy, visited_check, ix, iy, input_list)
+    print(f'{answer}')
+    
+
+
