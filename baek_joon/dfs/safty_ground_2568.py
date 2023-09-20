b97083a871abdf51324a34f387fdbcdb0dc6c040
choss001 <choss01@naver.com>
2023-09-20 14:00:18 +0900

    safty ground problem algorithm finish!

---
+++ baek_joon/dfs/safty_ground_2568.py
@@ -0,0 +1,49 @@
+import sys
+input = sys.stdin.readline
+sys.setrecursionlimit(10*10^100)
+
+move_x = [0, 1, 0, -1]
+move_y = [-1, 0, 1, 0]
+
+m = int(input())
+
+input_list = [ list(map(int, input().split())) for _ in range(m)]
+
+
+def dfs(ix, iy, height, visited_check):
+
+    for next_num in range(4):
+        dx = ix + move_x[next_num]
+        dy = iy + move_y[next_num]
+
+        if dx < 0 or dx >= m or dy < 0 or dy >=m:
+            continue
+        if input_list[dy][dx] < height:
+            continue
+        if visited_check[dy][dx] == 1:
+            continue
+        visited_check[dy][dx] = 1
+        dfs(dx, dy, height, visited_check)
+
+def graph_exam(height):
+    temp_answer = 0
+    visited_check = [[0] * m for _ in range(m)]
+    for ix in range(m):
+        for iy in range(m):
+            if input_list[iy][ix] >= height and \
+                    visited_check[iy][ix] == 0:
+                    temp_answer += 1
+                    visited_check[iy][ix] = 1
+                    dfs(ix, iy, height, visited_check)
+
+    return temp_answer
+
+answer = 0
+for value in range(max(max(input_list))):
+    answer = max(graph_exam(value), answer)
+    
+print(f'{answer}')
+
+
+
+
