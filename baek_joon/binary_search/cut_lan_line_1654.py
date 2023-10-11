94f9578f3e12c982477468c61277dbcac4965805
choss001 <choss01@naver.com>
2023-10-11 17:08:20 +0900

    cut lan line problem finish!

--- baek_joon/binary_search/cut_lan_line_1654.py
+++ baek_joon/binary_search/cut_lan_line_1654.py
@@ -14,12 +14,16 @@ def binary_search(start, end):
     while start <= end :
         temp_answer = 0
         mid = (start + end) // 2
+        print(f'start = {start}, end = {end}, mid = {mid}')
         for item in input_lst:
             temp_answer += item//mid
+            print(f'item = {item}, temp_answer = {item//mid}')
         
         if temp_answer >= m :
+            print(f'temp_answer >= m,  temp_answer = {temp_answer} , m = {m}')
             start = mid +1
         else :
+            print(f'temp_answer < m,  temp_answer = {temp_answer} , m = {m}')
             end = mid -1
     
     #print(f'start = {start}, end = {end}')
