0a88c1f324c7ce9ba1573edbdfc9eaba822e1bb3
choss001 <choss01@naver.com>
2023-10-28 00:23:24 +0900

    dijkstar practice

--- baek_joon/dijkstra/dijkstra_1753_1.py
+++ baek_joon/dijkstra/dijkstra_1753_1.py
@@ -11,24 +11,31 @@ distance = [INF]*(v+1)
 for i in range(e):
     a,b,c = map(int, input().split())
     graph[a].append((b,c))
-
+print(graph)
 def dijstra(start):
     q = []
 
     heapq.heappush(q, (0, start))
     distance[start] = 0
-    print(f'heapq = {heapq}')
     
     while q:
+        list(q)
+        print(f'q = {q}')
+        heapq.heapify(q)
         dist, now = heapq.heappop(q)
+        print(f'dist={dist},now={now}')
 
         if distance[now] < dist:
+            print(f'distance[now]={distance[now]},dist={dist} distance row than dist so that coninue')
             continue
 
         for i in graph[now]:
             cost = dist + i[1]
+            print(f'dist={dist}, i={i}, distance[i[0]]={distance[i[0]]},cost={cost}')
             if cost < distance[i[0]]:
+                print(f'cost row than distance so pointer can here')
                 distance[i[0]] = cost
+                print(f'distance[{i[0]}]={distance}')
                 heapq.heappush(q, (cost, i[0]))
 
 dijstra(start)
