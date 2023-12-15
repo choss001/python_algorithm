a2238d5380ebc95344049ae8b830836858b50fa6
choss001 <choss01@naver.com>
2023-12-15 11:02:34 +0900

    N and M backtracking algorithm practice

---
+++ baek_joon/backtracking/N_and_M_1_15649.py
@@ -0,0 +1,19 @@
+import sys
+input = sys.stdin.readline
+
+
+def backtracking():
+    if len(array) == M:
+        print(' '.join(map(str,array)))
+        return
+    for i in range(1,N+1):
+        if i not in array:
+            array.append(i)
+            backtracking()
+            array.pop()
+
+
+N,M= map(int,input().split())
+array = []
+backtracking()
+
