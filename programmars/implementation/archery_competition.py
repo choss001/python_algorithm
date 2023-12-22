d9153dbe0e6052aebbc2bdcf29bc07bdfff9f535
choss001 <choss01@naver.com>
2023-12-22 11:25:09 +0900

    archery_competiton problem practice

---
+++ programmars/implementation/archery_competition.py
@@ -0,0 +1,22 @@
+import sys
+input = sys.stdin.readline
+
+#def solution(n, info):
+#    for i in range(11):
+#        temp_n = n
+#        for k in range(i,11):
+#            if info[i]
+#
+#
+#    return -1
+#n, info = 5, [2,1,1,1,0,0,0,0,0,0,0]	
+#solution(n,info)
+
+def solution():
+    def dfs():
+
+    n =5
+    temp_lst = [0]*n
+
+    return -1
+
