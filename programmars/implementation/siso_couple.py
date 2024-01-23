432e39b3a6eee41e23f985edbff5af3b8462ec9a
choss001 <choss01@naver.com>
2024-01-23 19:58:59 +0900

    practice

---
+++ programmars/implementation/siso_couple.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdine.readline
+
+answer_dic = {}
+
+def solution(weights):
+    for i in range(len(weights)):
+        for k in range(i+1, len(weights)):
+
+
+weights = [100,180,360,100,270]
+
