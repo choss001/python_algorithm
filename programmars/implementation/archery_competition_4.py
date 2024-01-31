068172d2acb9d1d32e9b95abbc832fe7cccb8db2
choss001 <choss01@naver.com>
2024-01-31 16:16:02 +0900

    archery_competition practice

--- programmars/implementation/archery_competition_4.py
+++ programmars/implementation/archery_competition_4.py
@@ -5,28 +5,31 @@ diff = 0
 
 def dfs(current, result, idx, n, info,current_sum, info_sum):
     global diff
-    if n == 0 and diff <= current_sum - info_sum:
+    if n == 0 and diff <= current_sum - info_sum and current_sum - info_sum != 0:
         diff = current_sum - info_sum
         result.append((current.copy(), diff))
-        print(f'result !!current = {current}, current_sum = {current_sum}, info_sum={info_sum}, diff={diff}')
         return result
-
+    print(f'current={current}, idx={idx}, n={n}, current_sum={current_sum}, info_sum={info_sum}, result={result}')
     for i in range(idx, 11):
-        if n < info[i]+1:
+        if n < info[i]+1 and i != 10:
             continue
-        n -= info[i] +1
-        current[i] = info[i] +1
+        if n >= info[i] +1:
+            n -= info[i] +1
+            current[i] = info[i] + 1
+        else:
+            current[i] = n
+            n = 0
+        #n -= info[i] +1 if n >= info[i]+1 else n
+        #current[i] = info[i] +1 if n >= info[i]+1 else n
         if info[i] !=0:
             info_sum -= 10-i
         current_sum += 10 -i
-        print(f'current = {current} i={i}, info_sum={info_sum}, 10-i={10-i}, current_sum={current_sum}')
         dfs(current, result, i+1, n, info, current_sum, info_sum)
-        n += info[i] +1
+        n += current[i]
         current[i] = 0
         if info[i] !=0:
             info_sum += 10-i
         current_sum -= 10-i
-        print(f'current = {current} i={i}, info_sum={info_sum}, 10+i={10-i} current_sum={current_sum}')
 
 
     return result
@@ -37,11 +40,16 @@ def solution(n, info):
         if info[i]:
             info_sum += 10-i
 
-    print(f'info_sum={info_sum}')
     result = dfs([0]*11, [], 0, n, info, 0, info_sum)
+    result.sort(key=lambda x : (-x[1], x[0]))
+
     for item in result:
         print(item)
-    return 0
+    return result[0][0] if result else [-1]
 
-n, info = 5, [2,1,1,1,0,0,0,0,0,0,0]
+#n, info = 5, [2,1,1,1,0,0,0,0,0,0,0]
+#n, info = 1, [1,0,0,0,0,0,0,0,0,0,0]
+n, info = 9, [0,0,1,2,0,1,1,1,1,1,1]
+#n, info = 10, [0,0,0,0,0,0,0,0,3,4,3]
+#n, info = 1, [1,0,0,0,0,0,0,0,0,0,0]
 print(f'solution = {solution(n, info)}')
