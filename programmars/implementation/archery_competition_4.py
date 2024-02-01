25cc2de763496e4cc00276057933cc017807053c
choss001 <choss01@naver.com>
2024-02-01 10:05:06 +0900

    modify code for som error

--- programmars/implementation/archery_competition_4.py
+++ programmars/implementation/archery_competition_4.py
@@ -5,13 +5,15 @@ diff = 0
 
 def dfs(current, result, idx, n, info,current_sum, info_sum):
     global diff
+    if n == 0:
+        print(f'result !! || {current}, diff = {current_sum - info_sum}, current_sum = {current_sum}, info_sum ={info_sum}')
     if n == 0 and diff <= current_sum - info_sum and current_sum - info_sum != 0:
         diff = current_sum - info_sum
         result.append((current.copy(), diff))
         return result
-    print(f'current={current}, idx={idx}, n={n}, current_sum={current_sum}, info_sum={info_sum}, result={result}')
     for i in range(idx, 11):
         if n < info[i]+1 and i != 10:
+            #print(f'info[i]+1 and i != 0   ||||  current={current}, idx={idx}, n={n}, current_sum={current_sum}, info_sum={info_sum}, result={result}')
             continue
         if n >= info[i] +1:
             n -= info[i] +1
@@ -19,11 +21,10 @@ def dfs(current, result, idx, n, info,current_sum, info_sum):
         else:
             current[i] = n
             n = 0
-        #n -= info[i] +1 if n >= info[i]+1 else n
-        #current[i] = info[i] +1 if n >= info[i]+1 else n
         if info[i] !=0:
             info_sum -= 10-i
         current_sum += 10 -i
+        #print(f'current={current}, idx={idx}, n={n}, current_sum={current_sum}, info_sum={info_sum}, result={result}')
         dfs(current, result, i+1, n, info, current_sum, info_sum)
         n += current[i]
         current[i] = 0
@@ -31,6 +32,29 @@ def dfs(current, result, idx, n, info,current_sum, info_sum):
             info_sum += 10-i
         current_sum -= 10-i
 
+
+    return result
+
+def checker(result):
+    temp_high_score = 0
+    if result:
+        print(f'result = {result}')
+        temp_high_score = result[0][1]
+
+        temp_a = 0
+        temp_result = []
+        print(f'temp_high_score = {temp_high_score}')
+    
+        for item in result:
+            item = item[0]
+            print(f'item = {item}')
+            for i in range(10, -1, -1):
+                print(f'i = {i}')
+                if item[i] and item[i] > temp_a:
+                    temp_result = item
+                    temp_a = (i,item[i])
+                    break
+        result = temp_result
 
     return result
 
@@ -43,6 +67,9 @@ def solution(n, info):
     result = dfs([0]*11, [], 0, n, info, 0, info_sum)
     result.sort(key=lambda x : (-x[1], x[0]))
 
+    result = checker(result)
+    print(f'result = {result}')
+
     for item in result:
         print(item)
     return result[0][0] if result else [-1]
@@ -52,4 +79,6 @@ def solution(n, info):
 n, info = 9, [0,0,1,2,0,1,1,1,1,1,1]
 #n, info = 10, [0,0,0,0,0,0,0,0,3,4,3]
 #n, info = 1, [1,0,0,0,0,0,0,0,0,0,0]
+#n, info = 11, [1,1,1,1,1,1,1,1,1,1,1]
+
 print(f'solution = {solution(n, info)}')
