594774253861c7b63c8c3ff67b418d9c14558240
choss001 <choss01@naver.com>
2024-02-07 11:20:05 +0900

    tuple problem algorithm practice

--- programmars/implementation/tuple.py
+++ programmars/implementation/tuple.py
@@ -3,23 +3,19 @@ def solution(s):
     s.sort(key = lambda x : len(x))
 
     answer = []
+    print(f's = {s}')
     for item in s:
-        item = item.replace(",", "")
-        print(f'item={item}')
-        for k in answer:
-            item = item.replace(str(k),'')
-        answer.append(int(item))
-
-
-
-    print(f's={s}')
-    print(f'answer = {answer}')
-
+        item_lst = list(map(int,item.split(",")))
+        for k in item_lst:
+            if k in answer:
+                continue
+            answer.append(k)
 
     return answer
 #s = "{{1,2,3},{2,1},{1,2,4,3},{2}}"
 #s = "{{123}}"
 #s = "{{4,2,3},{3},{2,3,4,1},{2,3}}"
 #s = "{{1}}"
-s = "{{100000}}"
+#s = "{{100000}}"
+s =  "{{22},{2,22},{242,22,2}}"
 print(f'solution = {solution(s)}')
