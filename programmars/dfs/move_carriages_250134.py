5cf5be069639a72150e2004d9357810fc4ba3357
choss001 <choss01@naver.com>
2024-04-02 10:22:14 +0900

    move carriages practice

--- programmars/dfs/move_carriages_250134.py
+++ programmars/dfs/move_carriages_250134.py
@@ -1,10 +1,10 @@
 def solution(maze):
+    answer = 99999999
     move = [(-1,0),(0,1),(1,0),(0,-1)]
     start_r = [0,0]
     start_b = [0,0]
     v_r = [[False for _ in range(len(maze[0]))] for _ in range(len(maze))]
     v_b = [[False for _ in range(len(maze[0]))] for _ in range(len(maze))]
-    print(f'v_r={v_r}, v_b={v_b}')
 
     for i in range(len(maze)):
         for j in range(len(maze[0])):
@@ -12,12 +12,19 @@ def solution(maze):
                 start_r[0],start_r[1] = i,j
             if maze[i][j] == 2:
                 start_b[0],start_b[1] = i,j
-    print(f'start_b={start_b}, start_r={start_r}')
 
-    def dfs(start_r, start_b):
+    def dfs(start_r, start_b, count):
+        nonlocal answer
         v_r[start_r[0]][start_r[1]] = True
         v_b[start_b[0]][start_b[1]] = True
+        
+        if maze[start_r[0]][start_r[1]] == 3 and \
+                maze[start_b[0]][start_b[1]] == 4:
+                    answer = min(answer, count)
+                    print(f' answer = {answer}, count = {count}, start_r={start_r}, start_b={start_b}')
+                    return
 
+        print()
         print(f'svr, {start_r}' )
         for v in v_r:
             print(v)
@@ -25,30 +32,31 @@ def solution(maze):
         for v in v_b:
             print(v)
 
-
         for i in range(4):
             d_r = [start_r[0] + move[i][0], start_r[1] + move[i][1]]
+
+            if maze[start_r[0]][start_r[1]] == 3: d_r = start_r[0], start_r[1]
             if not (0 <= d_r[0] < len(maze)): continue
             if not (0 <= d_r[1] < len(maze[0])): continue
-            if v_r[d_r[0]][d_r[1]]: continue
+            if v_r[d_r[0]][d_r[1]] and maze[d_r[0]][d_r[1]] != 3 : continue
             if maze[d_r[0]][d_r[1]] == 5: continue
             if d_r[0] == start_b[0] and d_r[1] == start_b[1]: continue
             for j in range(4):
                 d_b = [start_b[0] + move[j][0], start_b[1] + move[j][1]]
+                if maze[start_b[0]][start_b[1]] == 4: d_b = start_b[0], start_b[1]
                 if not (0 <= d_b[0] < len(maze)): continue
                 if not (0 <= d_b[1] < len(maze[0])): continue
-                if v_b[d_b[0]][d_b[1]]: continue
-                dfs(d_r, d_b)
+                if v_b[d_b[0]][d_b[1]] and maze[d_b[0]][d_b[1]] != 4: continue
+                print(f'entrace before = d_r = {d_r}, d_b = {d_b}')
+                dfs(d_r, d_b, count + 1)
                 v_r[d_r[0]][d_r[1]] = False
                 v_b[d_b[0]][d_b[1]] = False
 
-    dfs(start_r, start_b)
-    
+    dfs(start_r, start_b, 0)
 
+    return answer if answer != 99999999 else 0
 
-
-    return
-
-maze = [[1, 4], [0, 0], [2, 3]]
+#maze = [[1, 4], [0, 0], [2, 3]]
+maze = [[1, 0, 2], [0, 0, 0], [5, 0, 5], [4, 0, 3]]
 print(solution(maze))
 
