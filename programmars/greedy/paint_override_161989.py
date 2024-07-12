e854674fd6031208a22e6a124ce92da93af50ecc
choss001 <choss01@naver.com>
2024-07-12 14:36:20 +0900

    I am solving paint_overrride problem

--- programmars/greedy/paint_override_161989.py
+++ programmars/greedy/paint_override_161989.py
@@ -1,6 +1,25 @@
 def solution(n, m, section):
+    
+    count = 0
+    end = 99999999
+    for i in range(len(section)):
+        start = i
+        end = start + m - 1
+        if start >= end :
+            continue
+        end = start + m - 1
+        if end > n :
+            count +=1
+            break
+        count +=1
+    return count
+
+
+
+
+        
 
 
 
 n,m,section=8,4,[2,3,6]
-section(n,m,section)
+print(solution(n,m,section))
